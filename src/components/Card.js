386841fc88bd446b56810aff060347ceb1af053f
Talha Zaigham <talha.zaigham2@gmail.com>
2023-02-06 21:22:25 +0100

    further work on FAQ section

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import Button from '@mui/material-next/Button';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,13 +11,18 @@ import '@fontsource/roboto/700.css';
 const Card = (props) => {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [downBtn, setDownBtn] = useState(true);
+
 
   return (
     <div className='card-container'>
 
-        <motion.div transition={{layout:{duration: 1, type: "spring"}}} layout onClick={()=>setIsOpen(!isOpen)} style={{borderRadius: "1rem", boxShadow: "0px 10px 30px rgba(33, 38, 43, 0.2)", minWidth: "40vh"}} className='card'>
+        <motion.div transition={{layout:{duration: 1, type: "spring"}}} layout onClick={()=>{setIsOpen(!isOpen);setDownBtn(!downBtn);}} style={{borderRadius: "1rem", boxShadow: "0px 10px 30px rgba(33, 38, 43, 0.2)", minWidth: "40vh"}} className='card'>
 
             <motion.h1 layout="position" >{props.title}</motion.h1>
+            {downBtn && (<motion.div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <ArrowDropDownIcon fontSize='large' />
+            </motion.div>)}
             {isOpen && 
             (<motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.7}} className='expand'>
 
@@ -24,7 +30,7 @@ const Card = (props) => {
                 {props.content}
               </p>
 
-              <Button variant='text' sx={{backgroundColor:'white', color: '#85929E', marginTop: '15px', alignItems: 'center'}}><KeyboardArrowRightIcon fontSize="small" sx={{opacity: '0'}}/>Explore<KeyboardArrowRightIcon fontSize="small"/></Button>
+              <Button className='card-btn' variant='text' ><KeyboardArrowRightIcon fontSize="small" sx={{opacity: '0'}}/>Explore<KeyboardArrowRightIcon fontSize="small"/></Button>
 
             </motion.div>)}
 
@@ -36,3 +42,5 @@ const Card = (props) => {
 }
 
 export default Card
+
+//style={{backgroundColor:'#85929E', color: '#21262b', marginTop: '15px', alignItems: 'center'}}
